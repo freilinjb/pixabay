26b93f82320a53a9e1c4be4b29979f66792a220f
freilinjb <freilinjb@gmail.com>
2020-04-14 17:52:44 -0400

    Create Componets Error and metho Validating of Form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Error from './Error';
 
 const Formulario = () => {
+
+    const [termino, setTermino] = useState('');
+    const [error, setError] = useState(false);
+
+    const buscarImagenes =e=> {
+
+        e.preventDefault();
+
+        //validar
+        if(termino.trim() === ''){
+            setError(true);
+            return;
+        }
+
+        setError(false);
+        //enviar el termino de busqueda hacia el componente principal
+
+    }
     return ( 
-        <form>
+        <form onSubmit={buscarImagenes}>
             <div className="row justify-content-center">
                 <div className="form-group col-md-8">
-                    <input type="text" name="buscador" className="form-control" placeholder="Buscador de Imagen, ejemplo: futbol o cafe"/>
+                    <input type="text" name="buscador" onChange={e =>setTermino(e.target.value)} className="form-control" placeholder="Buscador de Imagen, ejemplo: futbol o cafe"/>
                 </div>
                 <div className="form-group col-md-4">
-                    <button type="button" name="" id="" className="btn btn-primary btn-block">Consultar</button>
+                    <button type="submit" className="btn btn-primary btn-block">Consultar</button>
                 </div>
             </div>
+            { error ? <Error mensaje="Agrergue un término de búsqueda"/> : null }
+
         </form>
      );
 }
